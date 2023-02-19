a4b3b221e9912eaff5510787a9928090e3e868f2
Mindy Long <mindyflong@gmail.com>
2023-02-19 00:47:15 -0800

    upload image✅

--- client/components/UploadButton.js
+++ client/components/UploadButton.js
@@ -2,16 +2,23 @@ import {useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
 import { Icon } from "semantic-ui-react";
 import Button from 'components/Button'
+import SearchApi from "../api/SearchApi";
+import toast from "react-hot-toast";
 
 // import entire SDK
 var AWS = require('aws-sdk');
-const imageURL = "https://treehacksdrip.s3.amazonaws.com/’";
+const imageURL = "https://treehacksdrip.s3.amazonaws.com/";
 var albumBucketName = "treehacksdrip";
 var bucketRegion = 'us-east-1';
 var IdentityPoolId = 'us-east-1:e7ba7d5b-e247-43f9-b6d8-0bcfb3fdc4ad';
 
 export default function UploadButton() {
     const [invalid, setInvalid] = useState(false);
+
+    const postS3 = (photokey) => {
+        console.log(photokey);
+        const uploadPromise = SearchApi.postImage(photokey);
+    }
 
     AWS.config.update({
         region: bucketRegion,
@@ -25,23 +32,22 @@ export default function UploadButton() {
         params: { Bucket: albumBucketName }
     });
     
-    const uploadImage = (albumName) => {
+    const uploadImage = () => {
         var files = document.getElementById("photoupload").files;
         if (!files.length) {
             return alert("Please choose a file to upload first.");
         }
         var file = files[0];
         var fileName = file.name;
-        var albumPhotosKey = encodeURIComponent(albumName) + "/";
-        var image_key = str(uuidv4()) + ".";
+        var imageKey = String(uuidv4()) + ".";
         
-        var photoKey = albumPhotosKey + image_key + fileName;
+        var photoKey = encodeURI(imageURL + imageKey + fileName);
         
         // Use S3 ManagedUpload class as it supports multipart uploads
         var upload = new AWS.S3.ManagedUpload({
             params: {
                 Bucket: albumBucketName,
-                Key: photoKey,
+                Key: imageKey + fileName,
                 Body: file
             }
         });
@@ -53,6 +59,7 @@ export default function UploadButton() {
                 alert("Successfully uploaded photo.");
                 
                 // Post to API
+                postS3(photoKey);
             },
             function(err) {
                 return alert("There was an error uploading your photo: ", err.message);
@@ -67,7 +74,7 @@ export default function UploadButton() {
             icon
             labelPosition="left"
             htmlFor="project-form-image-input"
-            onClick={uploadImage}
+            onClick={() => uploadImage()}
           >
             <Icon name="upload" />
             Choose File
@@ -77,7 +84,7 @@ export default function UploadButton() {
             className="hidden-file-input"
             name="image"
             style={{ border: "none" }}
-            accept="image/*"
+            accept="image/*,video/mp4,video/x-m4v,video/*"
             type="file"
           />
         </div>
